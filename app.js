32b6697c16077e79e187bb9ecba5f3b980a556b2
michael.l.grande@gmail.com <mgrande@google.com>
2020-06-26 12:36:32 +0900

    Adds basic integration to stocks API

--- app.js
+++ app.js
@@ -2,12 +2,25 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const port = 3000
+const finnhub = require('finnhub');
+var util = require('util');
 
-let terms = ['Rob', 'Michael']
+const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+api_key.apiKey = "brqma3frh5rce3ls8mk0" // Replace this
+const finnhubClient = new finnhub.DefaultApi()
+
 let visitCount = 0
 
-app.get('/', (req, res) => {
-    res.send('Hello, ' + terms[visitCount % 2])
+app.get('/StockNews/:tickerSymbol', (req, res) => {
+    //Company News
+    finnhubClient.companyNews(req.params["tickerSymbol"], "2020-01-01", "2020-05-01", (error, data, response) => {
+        if (error) {
+            console.error(error);
+        } else {
+            const prettyVersion = data.map(dataMember => `<img width="25%" src="${dataMember.image}" /><a href="${dataMember.url}" >${dataMember.headline}</b><br/>`).join('');
+            res.send(prettyVersion);
+        }
+    });
     visitCount++
 })
 
